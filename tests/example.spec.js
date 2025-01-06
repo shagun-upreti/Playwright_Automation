d29ec7c6c45c2bfdefba9eadbf3de09da921a45b
shagun-upreti <shagunupreti1237@gmail.com>
2025-01-07 00:31:15 +0530

    page object

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,27 +1,31 @@
-import {test, expect} from "playwright/test"
-test('Client App login', async ({page})=>
-{
-    const productname="IPHONE 13 PRO";
-    const product=page.locator(".card-body");
-    await page.goto("https://rahulshettyacademy.com/client",{ timeout: 300000 });
-    await page.locator("#userEmail").fill("divya@yopmail.com");
-    await page.locator("#userPassword").fill("Apple123#");
-    await page.locator("#login").click();
-    await page.waitForLoadState('networkidle');
-    const titles= await page.locator(".card-body b").allTextContents();
+import { test, expect } from "playwright/test"
+const { LoginPage } = require('../pageobjects/LoginPage');
+
+
+test('Client App login', async ({ page }) => {
+    test.setTimeout(90000);
+    const username = "anshika@gmail.com";
+    const password = "Iamking@000";
+    const productname = "IPHONE 13 PRO";
+    const product = page.locator(".card-body");
+    const loginPage = new LoginPage(page);
+    loginPage.goTo();
+    loginPage.validLogin(username, password);
+    await page.waitForLoadState('networkidle', { timeout: 60000 });
+    const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
-    const count=await product.count();
-    for(let i=0;i<count;i++){
-        if(await product.nth(i).locator("b").textContent()=== productname) {
+    const count = await product.count();
+    for (let i = 0; i < count; i++) {
+        if (await product.nth(i).locator("b").textContent() === productname) {
 
             await product.nth(i).locator("text= Add To Cart").click();
             break;
         }
     }
-   
+
     await page.locator("[routerlink*='cart']").click();
     await page.locator("div li").first().waitFor();
-    const bool=await page.locator("h3:has-text('IPHONE 13 PRO')").isVisible();
+    const bool = await page.locator("h3:has-text('IPHONE 13 PRO')").isVisible();
     expect(bool).toBeTruthy();
 
     await page.locator("//button[text()='Checkout']").click();
